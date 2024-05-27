b3e52b65a430b1b871d761afb1a8b88f2b383e77
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 11:01:11 +0900

    컨트롤러 설계

--- src/main/java/org/choongang/global/AbstractController.java
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -1,6 +1,14 @@
 package org.choongang.global;
 
+import java.util.Scanner;
+
 public abstract class AbstractController implements Controller {
+
+    protected Scanner sc;
+
+    public AbstractController() {
+        sc = new Scanner(System.in);
+    }
 
     /**
      * 상단 공통 출력 부분
@@ -13,11 +21,15 @@ public abstract class AbstractController implements Controller {
      * 입력 항목
      */
     public void prompt() {
+        System.out.print("메뉴 선택: ");
+        String menu = sc.nextLine();
 
     }
 
     @Override
     public final void run() {
-
+        common();
+        show();
+        prompt();
     }
 }
