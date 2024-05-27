ce142639150c4bb377d770250b0df7ff646840df
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 11:35:12 +0900

    컨트롤러 설계

---
+++ src/main/java/org/choongang/member/controllers/JoinController.java
@@ -0,0 +1,14 @@
+package org.choongang.member.controllers;
+
+import org.choongang.global.AbstractController;
+
+/**
+ * 회원 가입 컨트롤러
+ *
+ */
+public class JoinController extends AbstractController {
+    @Override
+    public void show() {
+        System.out.println("회원가입!");
+    }
+}
---
+++ src/main/java/org/choongang/member/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package org.choongang.member.controllers;
+
+import org.choongang.global.AbstractController;
+
+/**
+ * 로그인 컨트롤러
+ *
+ */
+public class LoginController extends AbstractController {
+
+    @Override
+    public void show() {
+        System.out.println("로그인!");
+    }
+}
