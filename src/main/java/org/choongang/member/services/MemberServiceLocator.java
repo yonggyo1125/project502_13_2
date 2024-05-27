87f9a903ace285d563a943f79a8db868e211eda6
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 16:41:44 +0900

    회원 가입 기능 구현

--- src/main/java/org/choongang/member/services/MemberServiceLocator.java
+++ src/main/java/org/choongang/member/services/MemberServiceLocator.java
@@ -1,14 +1,24 @@
 package org.choongang.member.services;
 
+import org.choongang.global.AbstractServiceLocator;
 import org.choongang.global.Service;
 import org.choongang.global.ServiceLocator;
 import org.choongang.global.constants.Menu;
 
-public class MemberServiceLocator implements ServiceLocator {
-
+public class MemberServiceLocator extends AbstractServiceLocator {
 
     @Override
     public Service find(Menu menu) {
-        return null;
+        Service service = services.get(menu);
+        if (service != null) {
+            return service;
+        }
+
+        switch (menu) {
+            case JOIN: service = new JoinService(); break;
+            case LOGIN: service = new LoginService(); break;
+        }
+
+        return service;
     }
 }
