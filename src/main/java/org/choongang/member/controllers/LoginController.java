b81eb8ee5d3ac195d1c361edcff09680a993f737
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 16:23:58 +0900

    회원 가입 기능 구현

--- src/main/java/org/choongang/member/controllers/LoginController.java
+++ src/main/java/org/choongang/member/controllers/LoginController.java
@@ -2,6 +2,7 @@ package org.choongang.member.controllers;
 
 import org.choongang.global.AbstractController;
 import org.choongang.global.constants.Menu;
+import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
 
 /**
@@ -14,5 +15,22 @@ public class LoginController extends AbstractController {
     public void show() {
 
         Templates.getInstance().render(Menu.LOGIN);
+    }
+
+    @Override
+    public void prompt() {
+        String userId = promptWithValidation("아이디: ", s -> !s.isBlank());
+        String userPw = promptWithValidation("비밀번호: ", s -> !s.isBlank());
+
+        RequestLogin form = RequestLogin.builder()
+                .userId(userId)
+                .userPw(userPw)
+                .build();
+
+        System.out.println(form);
+        // 로그인 처리 ...
+
+        MainRouter.getInstance().change(Menu.MAIN);
+
     }
 }
