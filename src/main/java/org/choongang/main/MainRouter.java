e23392525d2fe01b3ec88e22e5a1ec5f877e4062
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 11:45:10 +0900

    컨트롤러 설계

---
+++ src/main/java/org/choongang/main/MainRouter.java
@@ -0,0 +1,29 @@
+package org.choongang.main;
+
+import org.choongang.global.Controller;
+import org.choongang.global.Router;
+import org.choongang.global.constants.Menu;
+import org.choongang.main.controllers.MainController;
+import org.choongang.member.controllers.JoinController;
+import org.choongang.member.controllers.LoginController;
+
+public class MainRouter implements Router {
+    @Override
+    public void change(Menu menu) {
+        Controller controller = null;
+        switch(menu) {
+            case JOIN: controller = new JoinController(); break;
+            case LOGIN: controller = new LoginController(); break;
+            default: controller = new MainController();
+        }
+
+        controller.run(); // common(), show(), prompt()
+    }
+
+    @Override
+    public void start() {
+        while(true) {
+            change(Menu.MAIN); // 첫 화면은 메인 컨트롤러 출력 화면
+        }
+    }
+}
---
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -0,0 +1,14 @@
+package org.choongang.main.controllers;
+
+import org.choongang.global.AbstractController;
+
+/**
+ * 메인 컨트롤러
+ *
+ */
+public class MainController extends AbstractController {
+    @Override
+    public void show() {
+        System.out.println("메인 메뉴...");
+    }
+}
