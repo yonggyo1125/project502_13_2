7fe7143f135862e257366e6623e86fc43e68cf0f
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 10:55:23 +0900

    컨트롤러 설계

---
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -0,0 +1,23 @@
+package org.choongang.global;
+
+public abstract class AbstractController implements Controller {
+
+    /**
+     * 상단 공통 출력 부분
+     */
+    public void common() {
+
+    }
+
+    /**
+     * 입력 항목
+     */
+    public void prompt() {
+
+    }
+
+    @Override
+    public final void run() {
+
+    }
+}
