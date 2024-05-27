26fa0b67a3ce0d53eb0f87b835493f136ad8011b
yonggyo1125 <yonggyo00@kakao.com>
2024-05-27 11:26:27 +0900

    컨트롤러 설계

--- src/main/java/org/choongang/global/AbstractController.java
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -1,5 +1,7 @@
 package org.choongang.global;
 
+import org.choongang.global.constants.Menu;
+
 import java.util.Scanner;
 
 public abstract class AbstractController implements Controller {
@@ -19,17 +21,43 @@ public abstract class AbstractController implements Controller {
 
     /**
      * 입력 항목
+     *  - 문자: q, exit, quit - 종료
+     *  - 숫자: 메뉴 항목
      */
     public void prompt() {
         System.out.print("메뉴 선택: ");
         String menu = sc.nextLine();
+        if (menu.equals("q") || menu.equals("quit") || menu.equals("exit")) {
+            System.exit(0); // 0 - 정상 종료, 1 - 비정상 종료
+        }
 
+        try {
+            int m = Integer.parseInt(menu);
+            change(m); // 메뉴 변경
+        } catch (Exception e) {
+            System.out.println("메뉴는 숫자로 입력하세요.");
+        }
     }
 
+    /**
+     * 템플릿 메서드 패턴 : 특정 절차가 고정되어 있는 경우
+     *
+     */
     @Override
     public final void run() {
         common();
         show();
         prompt();
+    }
+
+    private void change(int menuNo) {
+        Menu menu = null;
+        switch(menuNo) {
+            case 1: menu = Menu.JOIN; break; // 회원가입
+            case 2: menu = Menu.LOGIN; break; // 로그인
+            default: menu = Menu.MAIN; // 메인 메뉴
+        }
+
+        // 메뉴 컨트롤러 변경 처리 - Router
     }
 }
