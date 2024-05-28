80c86981a069a9364b6c947bb3088665c3960e9f
yonggyo1125 <yonggyo00@kakao.com>
2024-05-28 09:02:34 +0900

    마이바티스 공통 설정 정리

--- src/main/java/org/choongang/member/mapper/MemberMapper.java
+++ src/main/java/org/choongang/member/mapper/MemberMapper.java
@@ -1,4 +1,15 @@
 package org.choongang.member.mapper;
 
+import org.choongang.member.controllers.SearchMember;
+import org.choongang.member.entities.Member;
+
+import java.util.List;
+
 public interface MemberMapper {
+    List<Member> getList(SearchMember search);
+    Member get(String userId);
+    Member get(long userNo);
+    long getTotal(SearchMember search);
+    int exist(String userId);
+
 }
