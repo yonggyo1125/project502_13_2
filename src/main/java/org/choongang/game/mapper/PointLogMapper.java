db8f64e2379aa73e13c5a8f58a8a2edebda36f5f
yonggyo1125 <yonggyo00@kakao.com>
2024-05-29 08:59:48 +0900

    포인트 기록

---
+++ src/main/java/org/choongang/game/mapper/PointLogMapper.java
@@ -0,0 +1,11 @@
+package org.choongang.game.mapper;
+
+import org.choongang.game.entities.PointLog;
+import org.choongang.game.entities.Rank;
+
+import java.util.List;
+
+public interface PointLogMapper {
+    int register(PointLog pointLog);
+    List<Rank> getRank();
+}
