38467f10ff8dd143a3ba0ab7a8dab5bb098916c2
yonggyo1125 <yonggyo00@kakao.com>
2024-05-29 15:37:56 +0900

    포인트 기록

--- src/main/java/org/choongang/game/controllers/RankingController.java
+++ src/main/java/org/choongang/game/controllers/RankingController.java
@@ -1,19 +1,29 @@
 package org.choongang.game.controllers;
 
 import org.choongang.game.constants.SubMenu;
+import org.choongang.game.entities.Rank;
+import org.choongang.game.services.GameServiceLocator;
 import org.choongang.global.AbstractController;
+import org.choongang.global.Service;
 import org.choongang.global.constants.MainMenu;
 import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RankingController extends AbstractController {
     @Override
     public void show() {
 
+        Service<List<Rank>> service = GameServiceLocator.getInstance().find(SubMenu.RANKING);
 
-        Templates.getInstance().render(SubMenu.RANKING, () -> {
-            return "처리된 랭킹 목록\n";
-        });
+        List<Rank> ranks = service.process();
+        String ranking = ranks.stream()
+                .map(r -> String.format("%d등 - %s(%s)/%d점", r.getRank(), r.getUserNm(), r.getUserId(), r.getTotal()))
+                        .collect(Collectors.joining("\n"));
+
+        Templates.getInstance().render(SubMenu.RANKING, () -> ranking);
 
         // 랭킹 출력 후 게임 메뉴 이동
         MainRouter.getInstance().change(MainMenu.GAME);
