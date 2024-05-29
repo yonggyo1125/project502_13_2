b38a9180beae0e11964dc14212a3df01d3bc2135
yonggyo1125 <yonggyo00@kakao.com>
2024-05-29 14:39:11 +0900

    포인트 기록

--- src/test/java/org/choongang/game/mapper/PointLogMapperTest.java
+++ src/test/java/org/choongang/game/mapper/PointLogMapperTest.java
@@ -1,12 +1,14 @@
 package org.choongang.game.mapper;
 
 import org.apache.ibatis.session.SqlSession;
+import org.choongang.game.entities.PointLog;
 import org.choongang.global.configs.DBConn;
 import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mapper.MemberMapper;
 import org.choongang.member.services.JoinService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,7 +42,20 @@ public class PointLogMapperTest {
 
     }
 
+    @Test
     void mapperTest1() {
+        for (Member member : members) {
+            for (int i = 0; i < 50; i++) {
+                int point = (int)(Math.random() * 3 - 1) * 10; // -10, 0, 10
 
+                PointLog item = PointLog.builder()
+                        .point(point)
+                        .userNo(member.getUserNo())
+                        .message("테스트 포인트")
+                        .build();
+
+                mapper.register(item);
+            } // endfor
+        } // endfor
     }
 }
