31759cf45dd61036c8b97b1c1a025eff8b08d25b
yonggyo1125 <yonggyo00@kakao.com>
2024-05-30 17:53:20 +0900

    소켓

---
+++ src/main/java/org/choongang/global/socket/Ex02.java
@@ -0,0 +1,20 @@
+package org.choongang.global.socket;
+
+import java.time.LocalDateTime;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Client c1 = new Client("user02", s -> {
+            System.out.println(s);
+        });
+
+        while(true) {
+            SocketData data = new SocketData("user02", "all", "테스트", LocalDateTime.now());
+
+            c1.send(data);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
