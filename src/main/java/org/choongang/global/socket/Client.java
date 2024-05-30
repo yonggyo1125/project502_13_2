d49fb85b9cfd0bd4a0ac0695e3c51bf2aa49b34b
yonggyo1125 <yonggyo00@kakao.com>
2024-05-30 17:33:15 +0900

    소켓

--- src/main/java/org/choongang/global/socket/Client.java
+++ src/main/java/org/choongang/global/socket/Client.java
@@ -1,4 +1,56 @@
 package org.choongang.global.socket;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 public class Client {
+    private Socket socket;
+    private String name;
+    private ObjectMapper om;
+
+    public Client(String name, Consumer<SocketData> handler) {
+        this.name = name;
+        om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        String serverAddress = Objects.requireNonNullElse(System.getenv("server"), "127.0.0.1");
+        String _port = System.getenv("port");
+        int port = _port == null || _port.isBlank() ? 9999 : Integer.parseInt(_port);
+
+        try {
+            socket = new Socket(serverAddress, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * 수신 데이터 처리
+         *
+         */
+        Thread inputTh = new Thread(() -> {
+            try (DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+
+                while(true) {
+                    if (socket == null || socket.isClosed() || handler == null) {
+                        Thread.currentThread().yield();
+                        break;
+                    }
+
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+
 }
